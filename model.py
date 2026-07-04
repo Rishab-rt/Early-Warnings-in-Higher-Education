c7600318a2c6f63a927fed64d09820e7a55054dd
Rishab-rt <rtalusani07@gmail.com>
2026-07-03 22:20:03 -0400

    Model imports

---
+++ model.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+import joblib
